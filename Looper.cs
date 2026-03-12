c069be3c92f90eeaed81a048075c271fbea89e1e
Nishan Reddy <nishanr0202@gmail.com>
2026-03-12 14:43:18 -0700

    Add extra precautions to make sure looper functions properly
    Initialize timer to zero in OnStateEnter method.

--- Looper.cs
+++ Looper.cs
@@ -8,6 +8,12 @@ public class Looper : StateMachineBehaviour
   private float loopDuration = 30f; //30 seconds
   private float timer = 0f;
 
+  override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+  {
+    anim = animator;
+    timer = 0f; //make sure timer is zeroed
+  }
+
   override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
   {
     timer += Time.deltaTime;
